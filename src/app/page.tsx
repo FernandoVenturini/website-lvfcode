0e9ad66ee1b5a69c280e03557b7f49c0e78bbe18
Fernando <fernandoventurini9@hotmail.com>
2025-03-21 09:56:53 +0000

    added image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
           <SectionTwoBannersContent>
             <Image
               className="img_banner"
-              src="/images/img_01.webp"
+              src="https://coffee-shoppage.netlify.app/"
               alt="Logo"
               width={300}
               height={300}
@@ -78,7 +78,7 @@ export default function Home() {
           <SectionTwoBannersContent>
             <Image
               className="img_banner"
-              src="https://elements-resized.envatousercontent.com/elements-cover-images/13eddf87-e45c-4127-9790-8e4cd68c1e10?w=710&cf_fit=scale-down&q=85&format=auto&s=57b2ed5787ddbb79fb462bfa23c8fb8aed82ca96480dd42b4d206b9bf6e74576"
+              src=""
               alt="Logo"
               width={300}
               height={300}
@@ -91,7 +91,7 @@ export default function Home() {
           <SectionTwoBannersContent>
             <Image
               className="img_banner"
-              src="https://elements-resized.envatousercontent.com/elements-cover-images/0f4e235d-4870-4806-b5fe-0454cbedaca1?w=710&cf_fit=scale-down&q=85&format=auto&s=1934049c8eefad2094f7aaa2cceb19b494483772a8f501a2cc42ba9e0bfbf17e"
+              src=""
               alt="Logo"
               width={300}
               height={300}
@@ -104,7 +104,7 @@ export default function Home() {
           <SectionTwoBannersContent>
             <Image
               className="img_banner"
-              src="https://elements-resized.envatousercontent.com/elements-cover-images/fad1180c-0746-4330-b74e-9f82fc311206?w=710&cf_fit=scale-down&q=85&format=auto&s=bab8bc9067da670368bd4aae6e4bc13c26a756e79c5f5c354faa5010c6a323d5"
+              src=""
               alt="Logo"
               width={300}
               height={300}
@@ -117,7 +117,7 @@ export default function Home() {
           <SectionTwoBannersContent>
             <Image
               className="img_banner"
-              src="https://elements-resized.envatousercontent.com/elements-cover-images/92c466b1-4885-4368-b49c-6ee4884310b4?w=710&cf_fit=scale-down&q=85&format=auto&s=8095778987455f58319aaee36a9fb487d77a0a4ee15f0a4e246dda8f5283d5b6"
+              src=""
               alt="Logo"
               width={300}
               height={300}
@@ -130,7 +130,7 @@ export default function Home() {
           <SectionTwoBannersContent>
             <Image
               className="img_banner"
-              src="https://elements-resized.envatousercontent.com/elements-cover-images/611bcc23-f289-499d-baf8-5ef631388492?w=710&cf_fit=scale-down&q=85&format=auto&s=12ffb72c3916bc45ead87bea7d3e5c00eba82bd4cae23d847ce171bf07567f71"
+              src=""
               alt="Logo"
               width={300}
               height={300}
